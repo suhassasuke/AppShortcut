7c926ebb9df83b1b1714a44c2f45174396198107
suhassasuke <suhassasuke4090@gmail.com>
2020-01-29 00:54:18 +0530

    Initial commit

---
+++ app/src/main/java/com/wnet/appshortcut/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.wnet.appshortcut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
